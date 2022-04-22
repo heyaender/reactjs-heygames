5ac078cdeda39e76475d87290274b3687039db58
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2022-04-23 02:08:01 +0700

    index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useEffect } from 'react'
+import AOS from 'aos';
 
 const Home: NextPage = () => {
+    useEffect(() => {
+        AOS.init();
+    }, [])
 return (
     <>
     {/* <!-- Navbar --> */}

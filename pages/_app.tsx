9312e5f066172879eea5356c7e718015a5e8d0ba
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2022-04-24 03:12:00 +0700

    aos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         rel="stylesheet"/>
 
     {/* AOS Animations */}
-    <link href="https://unpk  g.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     
     {/* Call Bootstrap CSS */}

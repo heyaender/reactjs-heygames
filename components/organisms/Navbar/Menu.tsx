1a9d8ca02efd27665a3a5c1f8025b46e1cfcf36f
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2022-04-26 14:12:31 +0700

    refractor login navbar component

--- components/organisms/Navbar/Menu.tsx
+++ components/organisms/Navbar/Menu.tsx
@@ -1,12 +1,19 @@
+/* It's importing the classNames and Link from the next/link library. */
 import classNames from 'classnames'
 import Link from 'next/link'
 
+/* Defining the props that the component will take. */
 interface MenuProps {
     title: string
     active?: boolean
     href: string
 }
 
+/**
+ * It's a React component that renders a menu item
+ * @param props - Partial<MenuProps>
+ * @returns A menu item with a link to the href.
+ */
 export default function Menu(props: Partial<MenuProps>) {
     const { title, active, href = '/' } = props
     const classTitle = classNames({
@@ -14,6 +21,7 @@ export default function Menu(props: Partial<MenuProps>) {
         active,
     })
 
+    /* It's returning a menu item with a link to the href. */
     return (
         <li className="nav-item mxy-auto">
             <Link href={href}>

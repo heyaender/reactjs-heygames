55f45f8fe2ce16370ee747f664d8d7c7a9ebb5df
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2022-05-01 00:15:44 +0700

    refractor featured game components

--- components/molecules/GameItem/index.tsx
+++ components/molecules/GameItem/index.tsx
@@ -1,37 +1,50 @@
-export default function GameItem() {
+import Image from 'next/image'
+import Link from 'next/link'
+
+interface GameItemProps {
+    title: string
+    category: string
+    thumbnail: string
+}
+
+export default function GameItem(props: GameItemProps) {
+    const { title, category } = props
     return (
         <>
             <div className="featured-game-card position-relative">
-                <a href="./src/detail.html">
-                    <div className="blur-sharp">
-                        <img
-                            src="/img/Thumbnail-1.png"
-                            width="205"
-                            height="270"
-                            alt=""
-                        />
-                    </div>
-                    <div className="cover position-absolute bottom-0 m-32">
-                        <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
-                            <div className="game-icon mx-auto">
-                                <img
-                                    src="/icon/joystick.svg"
-                                    width={54}
-                                    height={36}
-                                    alt="game-icon"
-                                />
-                            </div>
-                            <div>
-                                <p className="fw-semibold text-white text-xl m-0">
-                                    Super Mechs
-                                </p>
-                                <p className="fw-light text-white m-0">
-                                    Mobile
-                                </p>
+                <Link href={'/detail'}>
+                    <a>
+                        <div className="blur-sharp">
+                            <img
+                                className="thumbnail"
+                                src={`/img/${props.thumbnail}.png`}
+                                width="205"
+                                height="270"
+                                alt="thumbnail-games"
+                            />
+                        </div>
+                        <div className="cover position-absolute bottom-0 m-32">
+                            <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
+                                <div className="game-icon mx-auto">
+                                    <Image
+                                        src="/icon/joystick.svg"
+                                        width={54}
+                                        height={36}
+                                        alt="game-icon"
+                                    />
+                                </div>
+                                <div>
+                                    <p className="fw-semibold text-white text-xl m-0">
+                                        {title}
+                                    </p>
+                                    <p className="fw-light text-white m-0">
+                                        {category}
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
+                    </a>
+                </Link>
             </div>
         </>
     )

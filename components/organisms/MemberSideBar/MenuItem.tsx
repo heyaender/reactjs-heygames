4df38452bae10e73e5099247f5b51405d23dbdfe
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2022-05-11 15:15:26 +0700

    refractor member sidebar overview

--- components/organisms/MemberSideBar/MenuItem.tsx
+++ components/organisms/MemberSideBar/MenuItem.tsx
@@ -1,20 +1,25 @@
+import classNames from 'classnames'
 import Image from 'next/image'
 
 interface MenuItemProps {
     title: string
+    icon: string
+    active?: boolean
 }
 
-export default function MenuItem(props: MenuItemProps) {
-    const { title } = props
+export default function MenuItem(props: Partial<MenuItemProps>) {
+    const { title, icon, active } = props
+    const classItem = classNames({
+        item: true,
+        'mb-30': true,
+        active: active,
+    })
     return (
         <>
-            <div className="item active mb-30">
-                <img
-                    className="item mb-30"
-                    src="/icon/SideOverview.svg"
-                    width={25}
-                    height={25}
-                />
+            <div className={classItem}>
+                <div className="me-3">
+                    <Image src={`/icon/${icon}.svg`} width={25} height={25} />
+                </div>
                 <p className="item-title m-0">
                     <a href="" className="text-lg text-decoration-none">
                         {title}
